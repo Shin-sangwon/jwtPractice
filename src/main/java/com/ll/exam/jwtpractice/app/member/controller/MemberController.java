853cfcd2ba5c2d93844f1beea9b53ef537f2e5ac
Shin-sangwon <dnjsml10@gmail.com>
2022-11-26 22:23:35 +0900

    feat : add id, password validation check

--- src/main/java/com/ll/exam/jwtpractice/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/jwtpractice/app/member/controller/MemberController.java
@@ -1,22 +1,39 @@
 package com.ll.exam.jwtpractice.app.member.controller;
 
+import com.ll.exam.jwtpractice.app.member.entity.Member;
+import com.ll.exam.jwtpractice.app.member.service.MemberService;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RequestMapping("/member")
 @RestController
 public class MemberController {
 
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
 
         if(loginDto.isNotValid()) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
+
+        Member member = memberService.findByUsername(loginDto.getUsername()).orElse(null);
+
+        if(member == null) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
+
+        if(!passwordEncoder.matches(loginDto.getPassword(), member.getPassword())) {
             return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
         }
 
